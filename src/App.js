b816ff5c19cb2f77aaa7dd2d734334fcbf93496e
cataopriscila <cataopriscila@gmail.com>
2021-09-17 17:45:05 -0300

    Fix: hardcoded API key

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Footer from "./Components/Footer/Footer";
 import CelebrityImage from "./Components/CelebrityImage/CelebrityImage";
 import TryAgain from "./Components/TryAgain/TryAgain";
 
-const API_KEY = process.env.RAPIDAPI_KEY;
+
 
 const particlesOptions = {
   particles: {
@@ -142,7 +142,7 @@ class App extends Component {
         method: "GET",
         headers: {
           "x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
-          "x-rapidapi-key": `${API_KEY}`,
+          "x-rapidapi-key": "ef03011c54msh503748a167509cbp1f8e61jsn0444855139ef",
         },
       }
     )
